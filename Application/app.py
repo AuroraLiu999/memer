642d891ab5433f8cff237fd1931af728ce415665
AuroraLiu999 <auroraliu999@gmail.com>
2025-03-24 22:43:12 -0400

    insert data

--- Application/app.py
+++ Application/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__) # port端口，起到服务器的作用
 UPLOAD_PATH = 'uploads' # 这行什么意思？设定变量，记录上传路径
 # create this directory if not exist
 
-app.config['UPLOAD_PATH'] = UPLOAD_PATH # 配置默认的上传地址，全局变量（在任何函数，任何页面都可以获取）# 这行什么意思？？？？？？？？？？？
+app.config['UPLOAD_PATH'] = UPLOAD_PATH # 配置默认的上传地址，全局变量（在任何函数，任何页面都可以获取
 app.secret_key = "123456" # 这行什么意思？flask的机制，保证应用安全
 
 
@@ -20,15 +20,14 @@ def index():
         login_result = login(username, password)
         if login_result is not None:
             session["username"] = username
-            return render_template('loginsuccess.html', username = session["username"]) # 26行也有一个return，这俩一样吗？？？？？？？？？作用分别是什么？？？？？？？？？？
-        else:
+            return render_template('loginsuccess.html', username = session["username"])
             flash("Incorrect Username or Password")
 
     print(os.getcwd())
 
     return render_template('mainpage.html') # 渲染模板，以后将mainpage装到这里
 
-@app.route('/signup', methods=["GET", "POST"]) # 每个函数前的这个网址是这个函数本身的网址还是这个函数结束后会跳转到的网址？？？？？？？？？？？函数本身
+@app.route('/signup', methods=["GET", "POST"]) # 每个函数前的这个网址是这个函数本身的网址还是这个函数结束后会跳转到的网址？函数本身
 def signup_page():
     if request.method == "POST":
         username = request.form["username"]
@@ -51,32 +50,21 @@ def signup_page():
 @app.route('/upload', methods=['GET','POST']) # 网站路径（upload的页面）
 def upload_file():
     if request.method == 'POST':
-
         if 'file' not in request.files: # 检查请求中是否包含文件
             return 'No File'
-
         print(request.files) # 打印请求中的文件信息
         file = request.files['file'] # 获取上传的文件对象
-
-        if not file.filename:
+        if not file.filename: #检查上传的文件是否有文件名
             return "No File Selected" # 并未上传文件
-
         if file:
             filename = file.filename # 设置文件名
             username = session["username"] # 从会话中获取用户名
             filepath = str(upload_meme(username, filename)) + ".jpg" # 生成文件路径
-            filepath = os.path.join(app.config['UPLOAD_PATH'], filepath) # 拼接文件路径
-          
-            file.save(filepath) # 保存文件到指定路径
-
-           
-            
-            return f'File {filename} uploaded successfully'
-
-            
+            filepath = os.path.join(app.config['UPLOAD_PATH'], filepath) # 拼接文件路径          
+            file.save(filepath) # 保存文件到指定路径         
+            return f'File {filename} uploaded successfully'     
         else:
             return 'File not valid' # 如果文件无效，返回提示信息
-
     return render_template("upload.html", username = session["username"]) # 如果是GET请求，渲染上传页面并传递用户名
 
 
@@ -84,7 +72,40 @@ def upload_file():
 def monkey():
     return render_template("monkey.html")
 
+@app.route('/chinese', methods=['GET'])
+def chinese():
+    return render_template("chinese.html")
+
+@app.route('/panda', methods=['GET'])
+def panda():
+    return render_template("panda.html")
+
+@app.route('/emotion', methods=['GET'])
+def emotion():
+    return render_template("emotion.html")
+
+@app.route('/chat', methods=['GET'])
+def chat():
+    return render_template("chat.html")
+
+@app.route('/cat', methods=['GET'])
+def cat():
+    return render_template("cat.html")
+
+@app.route('/bear', methods=['GET'])
+def bear():
+    return render_template("bear.html")
+
+@app.route('/dog', methods=['GET'])
+def dog():
+    return render_template("dog.html")
+
+@app.route('/trump', methods=['GET'])
+def trump():
+    return render_template("trump.html")
+
 
 
 if __name__ == '__main__':
     app.run(debug=True) # 启动Flask应用，开启调试模式
+
--- Application/helper_functions.py
+++ Application/helper_functions.py
@@ -5,7 +5,7 @@ from gridfs import GridFS
 
 # 连接到 MongoDB
 # connection_string_atlas = "mongodb+srv://auroraliu999:Lmy060912%40@cluster0.q6nk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-connection_string_local = "mongodb://localhost:27017/"
+connection_string_local = "mongodb://localhost:27017/" # 神马是本地？？？？？？？？？？
 client = MongoClient(connection_string_local)  # 默认连接到本地 MongoDB
 db = client["MemeGenSample"]  # 先选择使用哪个数据库
 
@@ -47,37 +47,13 @@ def upload_meme(username, filename):
     return insert_result.inserted_id
 
 
-# new_user = signup("abc","123")
-# assert new_user is not None
-# signup_res = signup("abc","123")
-# assert signup_res is None
-# login_res = login("abc","123")
-# assert login_res is not None
-# login_res = login("abc","321")
-# assert login_res is None
-# user_collection.delete_one({'_id': new_user})
+new_user = signup("abc","123")
+assert new_user is not None
+signup_res = signup("abc","123")
+assert signup_res is None
+login_res = login("abc","123")
+assert login_res is not None
+login_res = login("abc","321")
+assert login_res is None
+user_collection.delete_one({'_id': new_user})
 
-
-def put_meme_in_db():
-    # 初始化 GridFS
-    fs = GridFS(db)
-
-    # 遍历图片文件
-    for i in range(1, 44):
-        try:
-            # 读取图片文件
-            with open(f'meme{i}.jpg', 'rb') as f:
-                image_data = f.read()
-
-            # 将图片存入GridFS
-            file_id = fs.put(image_data, filename=f'meme{i}.jpg')
-            print(f"Image meme{i}.jpg stored with file_id: {file_id}")
-
-        except FileNotFoundError:
-            print(f"File meme{i}.jpg not found, skipping...")
-        except Exception as e:
-            print(f"An error occurred with meme{i}.jpg: {e}")
-
-        meme_collection.insert_one({"meme1.jpg": "hey"})
-
-put_meme_in_db()
